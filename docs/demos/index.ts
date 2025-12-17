4991a920ec9d371dfab65af8500918cc98ddd867
Giovambattista Fazioli <giovambattista.fazioli@gmail.com>
2025-12-17 14:04:29 +0100

    ♻️ refactor: remove maskBackground prop and background support
    - Drop background demo and docs example
    - Remove maskBackground prop from Mask component
    - Clean up styles, CSS, and story args
    - Update tests to reflect removal

--- docs/demos/index.ts
+++ docs/demos/index.ts
@@ -1,5 +1,4 @@
 export { maskActivation } from './Mask.demo.activation';
-export { maskBackground } from './Mask.demo.background';
 export { mask } from './Mask.demo.basic';
 export { maskEllipse } from './Mask.demo.ellipse';
 export { maskInvert } from './Mask.demo.invert';
